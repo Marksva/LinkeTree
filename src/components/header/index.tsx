24ae7fffea44a7a8d939ebd3d28b92ceeb7d5d52
mark <mark.sva@hotmail.com>
2025-10-07 00:06:11 -0300

    feat: add Header component with navigation and logout functionality

---
+++ src/components/header/index.tsx
@@ -0,0 +1,35 @@
+import { BiLogOut } from 'react-icons/bi'
+import { Link } from 'react-router-dom'
+
+import { auth } from '../../services/firebaseConnection'
+import { signOut } from 'firebase/auth'
+
+export function Header() {
+
+    async function handleLogout() {
+        await signOut(auth);
+    }
+
+
+    return (
+        <header className="w-full max-w-2x1 mt-4 px-1">
+            <nav className="w-full bg-white h-12 flex items-center justify-between rounded-md">
+                <div className="flex gap-4 font-medium">
+                    <Link to="/">
+                        Home
+                    </Link>
+                    <Link to="/admin">
+                        Admin
+                    </Link>
+                    <Link to="/admin/networks">
+                        Networks
+                    </Link>
+                </div>
+
+                <button title="Sair" onClick={handleLogout}>
+                    <BiLogOut size={28} color="#db2629" />
+                </button>
+            </nav>
+        </header>
+    )
+}
