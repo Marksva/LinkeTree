17d2e34eaa45345d577d07f7a9d0c7dadaa80fdd
mark <mark.sva@hotmail.com>
2025-10-14 15:19:20 -0300

    feat: implement link management with add and delete functionality in Admin page

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,18 +1,94 @@
-import { useState } from "react";
+import { useState, type FormEvent, useEffect } from "react";
 import { Header } from "../../components/header";
 import { Input } from "../../components/Input";
+import { FiTrash } from "react-icons/fi";
+import { db } from '../../services/firebaseConnection';
+import { addDoc, collection, onSnapshot, query, orderBy, deleteDoc, doc } from "firebase/firestore";
+
+interface LinkProps {
+    id: string;
+    name: string;
+    url: string;
+    bg: string;
+    color: string;
+}
 
 export function Admin() {
+
     const [nameInput, setNameInput] = useState("");
     const [urlInput, setUrlInput] = useState("");
     const [textColorInput, setTextColorInput] = useState("#f1f1f1");
     const [backgroundColorInput, setBackgroundColorInput] = useState("#121212");
 
+    const [links, setLinks] = useState<LinkProps[]>([]);
+
+    useEffect(() => {
+
+        const linkRef = collection(db, "links");
+        const queryRef = query(linkRef, orderBy("created", "asc"));
+
+        const unsub = onSnapshot(queryRef, (snapshot) => {
+
+            const lista = [] as LinkProps[];
+            snapshot.forEach((doc) => {
+                lista.push({
+                    id: doc.id,
+                    name: doc.data().name,
+                    url: doc.data().url,
+                    bg: doc.data().bg,
+                    color: doc.data().color
+                })
+            })
+
+            setLinks(lista);
+        })
+
+
+        return () => {
+            unsub();
+        }
+
+
+    }, [])
+
+    function handleRegister(e: FormEvent<HTMLFormElement>): void {
+        e.preventDefault();
+
+        if (nameInput === "" || urlInput === "") {
+            alert("Preencha todos os campos!");
+            return;
+        }
+
+        addDoc(collection(db, "links"), {
+            name: nameInput,
+            url: urlInput,
+            bg: backgroundColorInput,
+            color: textColorInput,
+            created: new Date()
+
+        }).then(() => {
+            setNameInput("");
+            setUrlInput("");
+            setTextColorInput("#f1f1f1");
+            setBackgroundColorInput("#121212");
+            console.log("Link cadastrado com sucesso!");
+        }).catch((error) => {
+            console.log("Erro ao cadastrar o link: ", error);
+        })
+
+        return;
+    }
+
+    async function handleDeleteLink(id: string) {
+        const docRef = doc(db, "links", id);
+        await deleteDoc(docRef);
+    }
+
     return (
         <div className="flex items-center flex-col min-h-screen pb-7 px-2">
             <Header />
 
-            <form className="flex flex-col mt-8 mb-3 w-full max-w-xl">
+            <form className="flex flex-col mt-8 mb-3 w-full max-w-xl" onSubmit={handleRegister}>
                 <label className="text-white font-medium mt-2 mb-2">Nome do Link</label>
                 <Input
                     type="text"
@@ -48,17 +124,49 @@ export function Admin() {
                     </div>
                 </section>
 
-                <div className="flex items-center justify-start flex-col mb-7 p-1 border-gray-100/25 border rounded-md">
-                    <label className="text-white font-medium mt-2 mb-2">Veja como está ficando:</label>
-                    <article
-                        className="w-11/12 max-w-lg flex flex-col items-center justifify-between bg-zinc-900 rounded-md px-1 py-3"
-                        style={{ marginBottom: 8, marginTop: 8, backgroundColor: backgroundColorInput, color: textColorInput }}
-                    >
-                        <p className="font-medium">{nameInput}</p>
-                    </article>
-                </div>
+                {nameInput !== "" && (
+                    <div className="flex items-center justify-start flex-col mb-7 p-1 border-gray-100/25 border rounded-md">
+                        <label className="text-white font-medium mt-2 mb-2">Veja como está ficando:</label>
+                        <article
+                            className="w-11/12 max-w-lg flex flex-col items-center justifify-between bg-zinc-900 rounded-md px-1 py-3"
+                            style={{ marginBottom: 8, marginTop: 8, backgroundColor: backgroundColorInput, color: textColorInput }}
+                        >
+                            <p className="font-medium">{nameInput}</p>
+                        </article>
+                    </div>
+                )}
+
+                <button
+                    type="submit"
+                    className="mb-7 bg-blue-600 h-9 rounded-md text-white font-medium gap-4 flex justify-center items-center"
+                >
+                    Cadastrar
+                </button>
 
             </form>
+
+            <h2 className="font-bold text-white mb-4 text-2xl">
+                Meus Links
+            </h2>
+
+            {links.map((link) => (
+                <article
+                    key={link.id}
+                    className="flex items-center justify-between w-11/12 max-w-xl rounded py-3 px-2 mb-2 select-none"
+                    style={{ backgroundColor: link.bg, color: link.color }}
+                >
+                    <p>{link.name}</p>
+                    <div>
+                        <button
+                            className="border border-dashed p-1 rounded bg-neutral-900"
+                            onClick={() => handleDeleteLink(link.id)}
+                        >
+                            <FiTrash size={18} color="#FFF" />
+                        </button>
+                    </div>
+                </article>
+            ))}
+
 
         </div>
     )
